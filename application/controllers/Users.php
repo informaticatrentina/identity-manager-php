896f96111df82b9a0736a11a71417fbce83c564c
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-18 17:36:25 +0200

    Fix

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -54,10 +54,20 @@ class Users extends REST_Controller
                 }
                 else
                 {
+                  if (is_array($where_string['$or']) && count($where_string['$or']) > 0)
+		              {
+	   	              $where_cond = array();			          
+	
+			              foreach ($where_string['$or'] as $wh => $val)
+			              {
+				              $where_cond[] = array($wh=>$val);
+			              }			             
+		              }
+                  file_put_contents('debug.log',print_r($where_cond,TRUE),FILE_APPEND);   
                   file_put_contents('debug.log','sono qui',FILE_APPEND);   
-                  $data=$this->mongo_db->where(array($where_string['$or']))->get('users');
-                  file_put_contents('debug.log','vuoto o pieno',FILE_APPEND);   
-                  file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);                  
+                  //$data=$this->mongo_db->where(array($where_string['$or']))->get('users');
+                  //file_put_contents('debug.log','vuoto o pieno',FILE_APPEND);   
+                  //file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);                  
                 }
               }
               
