8a7eb2bca3dc28dd1c00325f7ed23a5c1885114d
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-18 18:01:48 +0200

    sdfsd

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -57,7 +57,7 @@ class Users extends REST_Controller
                   if (is_array($where_string['$or']) && count($where_string['$or']) > 0)
 		              {
 	   	              file_put_contents('debug.log','sono qui',FILE_APPEND);   
-                    $data=$this->select('status')->mongo_db->where_or($where_string['$or'])->get('users');
+                    $data=$this->mongo_db->select('status')->where_or($where_string['$or'])->get('users');
                     file_put_contents('debug.log','vuoto o pieno',FILE_APPEND);   
                     file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);                 	             
 		              }              
