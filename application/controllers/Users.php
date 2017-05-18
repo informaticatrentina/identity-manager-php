ae7269010d3e675ffebe6f391ccecfc05c5cb622
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-18 18:00:21 +0200

    fix

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -56,23 +56,11 @@ class Users extends REST_Controller
                 {
                   if (is_array($where_string['$or']) && count($where_string['$or']) > 0)
 		              {
-	   	              $where_cond = array();               
-
-			              foreach ($where_string['$or'] as $key => $val)
-			              {
-                      if(is_array($val) && isset($val['_id']))
-                      {                        
-                        $val = new MongoId((string)$val['_id']);
-                      }                           
-                      array_push($where_cond,$val);				              
-			              }			             
-		              }
-                  file_put_contents('debug.log',print_r($where_cond,TRUE),FILE_APPEND);
-
-                  file_put_contents('debug.log','sono qui',FILE_APPEND);   
-                  $data=$this->mongo_db->where($where_cond)->get('users');
-                  file_put_contents('debug.log','vuoto o pieno',FILE_APPEND);   
-                  file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);                  
+	   	              file_put_contents('debug.log','sono qui',FILE_APPEND);   
+                    $data=$this->select('status')->mongo_db->where_or($where_string['$or'])->get('users');
+                    file_put_contents('debug.log','vuoto o pieno',FILE_APPEND);   
+                    file_put_contents('debug.log',print_r($data,TRUE),FILE_APPEND);                 	             
+		              }              
                 }
               }
               
