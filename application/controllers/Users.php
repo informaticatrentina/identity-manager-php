77e821904c76dcd04f95117bc588c2fc06a8686d
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-23 16:54:50 +0200

    sdfasdffasd

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -250,8 +250,10 @@ class Users extends REST_Controller
           if(preg_match('/(or:){1}/',$params['where']))
           {
             $params['where'] = str_replace('$or:', '', $params['where']);    
-            $params['where'] = str_replace('{[', '', $params['where']);    
-            $params['where'] = str_replace(']}', '', $params['where']);    
+            $params['where'] = str_replace('{[', '', $params['where']);                
+            $params['where'] = str_replace(']}', '', $params['where']); 
+            $params['where'] = str_replace('{', '', $params['where']); 
+            $params['where'] = str_replace('}', '', $params['where']);    
              
             file_put_contents('debug.log',print_r($params['where'],TRUE),FILE_APPEND);
           }
