5b4a89a3a619594e6343f08c059c713c00ec2c00
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-25 14:53:14 +0200

    ASDDFASD

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -56,176 +56,11 @@ class Users extends REST_Controller
               if(isset($dataprojection[0]) && $dataprojection[0]=='email') $dataprojection='email';
               if(isset($dataprojection[0]) && $dataprojection[0]=='_id') $dataprojection='_id';             
             }
-            file_put_contents('debug.log',print_r($where_string['$or'],TRUE),FILE_APPEND);
+           
             
-            /*
-            if(preg_match('/(or:){1}/',$params['where']))
-            {
-            $params['where'] = str_replace('$or:', '', $params['where']);    
-            $params['where'] = str_replace('{[', '', $params['where']);                
-            $params['where'] = str_replace(']}', '', $params['where']); 
-            $params['where'] = str_replace('{', '', $params['where']); 
-            $params['where'] = str_replace('}', '', $params['where']);    
-            $params['where'] = str_replace(',', '', $params['where']);   
-
-            // Ricerca tramite _id
-            if(preg_match('/(_id){1}/',$params['where']))
-            {
-              $params['where'] = str_replace('_id', '', $params['where']); 
-              $data=explode(":", $params['where']);              
-              $clean_data=array_filter($data);
-              $final_data=array_values($clean_data);
-
-              $where_conditions=array();
-
-              foreach($final_data as $val)
-              {
-                $where_conditions[]=new MongoId($val);                      
-              }
-
-              if($dataprojection=='email')
-              {
-                file_put_contents('debug.log','ciao',FILE_APPEND);
-                $data=$this->mongo_db->select(array('_id','email','_created','_updated','type'))->where_in('_id', $where_conditions)->get('users');            
-              }
-              else $data=$this->mongo_db->where_in('_id', $where_conditions)->get('users');    
-
-              if(!empty($data))
-              {
-                foreach($data as $key => $value)
-                {   
-                  if(isset($value['_created']))
-                  {                                                               
-                    date_default_timezone_set('Europe/Rome');                        
-                    $data[$key]['_created']=date('Y-m-d H:i:s',$value['_created']->sec);
-                  } 
-                  if(isset($value['_updated']))
-                  {                                                               
-                    date_default_timezone_set('Europe/Rome');                        
-                    $data[$key]['_updated']=date('Y-m-d H:i:s',$value['_updated']->sec);  
-                  } 
-                  if(isset($value['_id']))
-                  {
-                    $data[$key]['_id']=(string)$value['_id'];
-                  }
-                  if(isset($data[$key]['type']) && isset($data[$key]['_id']))
-                  {
-                    $data[$key]['_links']=array('self' => array('title' => $data[$key]['type'], 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'.$data[$key]['_id']));
-                    unset($data[$key]['type']);
-                  }
-                    
-                  // Elimino Password e ResetPwd
-                  unset($data[$key]['password']);
-                  unset($data[$key]['resetpwd']);          
-                }
-                
-                // Count data
-                $count=count($data);
-                
-                $data['_links']=array('self' => array('title' => 'users', 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'), 'parent' => array('href' => $_SERVER['SERVER_NAME'].'/v1', 'title' => 'home'));
-                $data['_meta']=array('max_results' => 25, 'total' => $count, 'page' => 1);
-
-              }                   
-              $this->response(array('_items' => $data), REST_Controller::HTTP_OK);
-              return;       
-            }
-
-            if(preg_match('/(email){1}/',$params['where']))
-            {
-              $params['where'] = str_replace('email', '', $params['where']); 
-              $data=explode(":", $params['where']);              
-              $clean_data=array_filter($data);
-              $final_data=array_values($clean_data);
-
-              $where_conditions=array();
-              foreach($final_data as $val)
-              {
-                $where_conditions[]=$val;                      
-              }
-  
-              $data=$this->mongo_db->select(array('_id','email','_created','_updated','type'))->where_in('email', $where_conditions)->get('users');          
-              
-              if(!empty($data))
-              {
-                foreach($data as $key => $value)
-                {   
-                  if(isset($value['_created']))
-                  {                                                               
-                    date_default_timezone_set('Europe/Rome');                        
-                    $data[$key]['_created']=date('Y-m-d H:i:s',$value['_created']->sec);
-                  } 
-                  if(isset($value['_updated']))
-                  {                                                               
-                    date_default_timezone_set('Europe/Rome');                        
-                    $data[$key]['_updated']=date('Y-m-d H:i:s',$value['_updated']->sec);  
-                  } 
-                  if(isset($value['_id']))
-                  {
-                    $data[$key]['_id']=(string)$value['_id'];
-                  }
-                    $data[$key]['_links']=array('self' => array('title' => $data[$key]['type'], 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'.$data[$key]['_id']));
-                    // Elimino Password e ResetPwd
-                    unset($data[$key]['password']);
-                    unset($data[$key]['resetpwd']);
-                }
-
-                // Count data
-                $count=count($data);                
-  
-                $data['_links']=array('self' => array('title' => 'users', 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'), 'parent' => array('href' => $_SERVER['SERVER_NAME'].'/v1', 'title' => 'home'));
-                $data['_meta']=array('max_results' => 25, 'total' => $count, 'page' => 1);
-              }                   
-              $this->response(array('_items' => $data), REST_Controller::HTTP_OK);
-              return;       
-            }      
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         /*
               if(isset($where_string['$or'][0]) && count($where_string['$or'][0]==1))
               {
-                
+                 file_put_contents('debug.log','SONO QUI',FILE_APPEND);
                 if(isset($where_string['$or'][0]['email']) && !empty($where_string['$or'][0]['email']))
                 {
                   return $this->_checkEmail($where_string['$or'][0]['email']);
@@ -269,8 +104,14 @@ class Users extends REST_Controller
                     return;                	             
 		              }              
                 }
-              }      
-              */      
+              } 
+              else file_put_contents('debug.log','SONO DI LA',FILE_APPEND);   
+
+
+
+
+
+
           
           }
 
