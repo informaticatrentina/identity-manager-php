77da04105b55cce8b5fd262f813b0ee018db9af1
Stefano Beccalli <stefano.beccalli@jlbbooks.it>
2017-05-26 10:02:36 +0200

    asdfdsf

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -94,10 +94,10 @@ class Users extends REST_Controller
                 }
                 
                 // Count data
-                //$count=count($data);
+                $count=count($data);
                 
-                $data['_links']=array('self' => array('title' => 'users', 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'), 'parent' => array('href' => $_SERVER['SERVER_NAME'].'/v1', 'title' => 'home'));
-                //$data['_meta']=array('max_results' => 25, 'total' => $count, 'page' => 1); 
+                //$data['_links']=array('self' => array('title' => 'users', 'href' => $_SERVER['SERVER_NAME'].'/v1/users/'), 'parent' => array('href' => $_SERVER['SERVER_NAME'].'/v1', 'title' => 'home'));
+                $data['_meta']=array('max_results' => '25', 'total' => '0', 'page' => '1'); 
                 
                 $this->response(array('_items' => $data), REST_Controller::HTTP_OK);                
                 return;
